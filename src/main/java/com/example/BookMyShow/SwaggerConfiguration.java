f6968fc5393023015de8ce8e475f475af74db5ef
Viraj-Pansuriya <virajpansuriya777@gmail.com>
2024-05-02 00:28:51 +0530

    testing

---
+++ src/main/java/com/example/BookMyShow/SwaggerConfiguration.java
@@ -0,0 +1,42 @@
+package com.example.BookMyShow;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@Configuration
+
+class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.BookMyShow"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+
+
+    private ApiInfo apiInfoMetaData() {
+
+        return new ApiInfoBuilder().title("BookMyShow")
+                .description("API Endpoint Decoration")
+                .contact(new Contact("Dev-Team", "https://www.dev-team.com/", "dev-team@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
+
+}
